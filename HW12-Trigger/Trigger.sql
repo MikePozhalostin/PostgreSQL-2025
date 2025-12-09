4c0e0d1dfbd91b1e7782a01a6171735aa5dc5e3b
Михаил Пожалостин <m.pozhalostin@whyhappen.ru>
2025-12-09 11:18:19 +0300

    fix insert sales

--- HW12-Trigger/Trigger.sql
+++ HW12-Trigger/Trigger.sql
@@ -120,3 +120,6 @@ SELECT G.good_name, sum(G.good_price * S.sales_qty)
 FROM goods G
 INNER JOIN sales S ON S.good_id = G.goods_id
 GROUP BY G.good_name;
+
+-- Добавление нового уникального индекса
+CREATE UNIQUE INDEX good_sum_mart_unq ON good_sum_mart (good_name);
